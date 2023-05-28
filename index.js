5a76aad55381a66dd7bf5670aa1d04aea567d5a4
Lete114 <me@imlete.cn>
2023-05-28 16:23:38 +0800

    feat: export database adapter

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 const server = require('./server')
 const main = require('./main')
+const adapter = require('./database/adapter')
 
-module.exports = { server, main }
+module.exports = { server, main, database: adapter }
